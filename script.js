ee30b3d8eaca884e0d7470341d667e4e0620a313
Colin Joss <colindjoss@gmail.com>
2021-05-26 11:22:08 -0700

    Fixed error w/ carousel animation

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var picture = document.getElementById('carousel');
-var allPhotos = ['images/pic_2.jpg', 'images/pic_3.jpg', 'images/pic_1.jpg'];
+var allPhotos = ['images/pic_1.jpg', 'images/pic_2.jpg', 'images/pic_3.jpg'];
 var index = 0;
 
 document.getElementById('button1').addEventListener('click', buttonRight);
 document.getElementById('button2').addEventListener('click', buttonLeft);
 
-window.setInterval(rotatePhoto, 6000);  // Rotates every 6 seconds
+window.setInterval(rotatePhoto, 5000);  // Rotates every 5 seconds
 
 function rotatePhoto() {
-    picture.setAttribute('src', allPhotos[index]);
     function updateIndex() {
         index++;
         if (index > 2) { index = 0 };  // Loop back to 0 if index exceeds 2
     };
     updateIndex();  // Closure means index keeps going up
+    picture.setAttribute('src', allPhotos[index]);
 };
 
 function buttonLeft () {
@@ -24,6 +24,7 @@ function buttonLeft () {
 
 function buttonRight () {
     index++;  // Increments index
+    console.log(index, allPhotos[index])
     if (index > 2) {index = 0};
     picture.setAttribute('src', allPhotos[index]);
 };
